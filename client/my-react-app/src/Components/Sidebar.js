0bc15b210010fa2c2a50e06b7ef265c1aaf1c267
Ms <125511982+mayamelissa15@users.noreply.github.com>
2024-04-26 08:30:30 +0000

    Sidebar done

--- client/my-react-app/src/Components/Sidebar.js
+++ client/my-react-app/src/Components/Sidebar.js
@@ -1,15 +1,27 @@
 import React from 'react';
-
+import add from '../Assets/Add.png';
+import Addition from '../Assets/Addition.png'; 
+import Settings from '../Assets/settings.png';
+import padlock from '../Assets/padlock.png';
 const Sidebar = () => {
   return (
     <div className="fixed left-0 top-0 h-full w-64  bg-slate-50 text-black flex flex-col justify-between rounded-3xl">
       <div className="py-4 px-6">
         <h1 className="text-xl font-bold">Sidebar</h1>
         <ul className="m-2 p-4">
-          <li className="p-2 cursor-pointer rounded transition-colors duration-300 bg-[#AA8CFF] text-white font-normal text-xl text-center p-4 rounded-xl">
-           New Project 
+        <li className=" cursor-pointer  flex items-center justify-around rounded transition-colors duration-300 bg-[#AA8CFF] text-white font-normal text-md text-center rounded-xl p-2">
+            <img src={add} className="justify-around" alt="Add" /> New Project 
           </li>
-          <li className="mt-4 text-center text-slate-400 font-normal  px-4 py-2 hover:bg-gray-100 cursor-pointer rounded-xl duration-500">My projects</li>
+
+          <li className="mt-4 text-center text-sm  font-medium  px-4 py-2 hover:bg-gray-200 cursor-pointer rounded-xl duration-500 flex items-center">
+            <img src={Addition} className="mr-2" alt="Add" /> My projects
+          </li>
+          <li className="mt-4 text-center text-sm font-medium  px-4 py-2 hover:bg-gray-200 cursor-pointer rounded-xl duration-500 flex items-center">
+            <img src={Settings} className="mr-2" alt="Add" /> Settings
+          </li>
+          <li className="mt-4 text-center text-sm font-medium  px-4 py-2 hover:bg-gray-200 cursor-pointer rounded-xl duration-500 flex items-center">
+            <img src={padlock} className="mr-2" alt="Add" />Policy
+          </li>
          
         </ul>
       </div>
