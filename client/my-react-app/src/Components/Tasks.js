0d3fec4f35569e5b89984e99eedd9cc919dd2513
Ms <125511982+mayamelissa15@users.noreply.github.com>
2024-04-26 22:59:26 +0000

    update

--- client/my-react-app/src/Components/Tasks.js
+++ client/my-react-app/src/Components/Tasks.js
@@ -1,14 +1,15 @@
 import TaskComplete from "./TaskComplete";
 
 const Tasks = () => {
-    return ( <div className="mx-64 p-16 bg-white rounded-md ">
-        <div className="bg-white shadow-xl rounded-md">
-            <h1 className="text-black text-2xl font-medium m-16 ">Completed</h1>
+    return ( <div className="  bg-white rounded-md ">
+        <div className="p-16 bg-white shadow-xl rounded-md">
+            <h1 className="text-black text-2xl font-medium mb-8 ">Completed</h1>
             <div className="grid grid-cols-2 gap-2  ">
             <TaskComplete title="Create fondation color"
     description="All about Foundation color for the designer easy to work"></TaskComplete>
   <TaskComplete title="Create fondation color"
-    description="All about Foundation color for the designer easy to work"></TaskComplete><TaskComplete title="Create fondation color"
+    description="All about Foundation color for the designer easy to work"></TaskComplete>
+    <TaskComplete title="Create fondation color"
     description="All about Foundation color for the designer easy to work"></TaskComplete>
   <TaskComplete title="Create fondation color"
     description="All about Foundation color for the designer easy to work"></TaskComplete>
--- client/my-react-app/src/Pages/UserMenu.js
+++ client/my-react-app/src/Pages/UserMenu.js
@@ -1,16 +1,25 @@
+import Navbar from "../Components/Navbar";
 import Sidebar from "../Components/Sidebar";
 import Tasks from "../Components/Tasks";
 import TaskUser from "../Components/TaskUser";
 
 const UserMenu = () => {
     return (
-        <div className="grid grid-cols-3 gap-6">
-            
-                    <Sidebar />
-                    
-               
-            
+        <div className="bg-[#F6F5FB] flex basis ">
+        <div className="basis-2/12">
+            <Sidebar></Sidebar>
         </div>
+        <div className="basis-8/12 bg-slate-50">
+            <div className="m-16">
+               
+            <Tasks></Tasks>
+            </div>
+           
+        </div>
+        <div className="basis-2/12 pt-4  pr-4 bg-slate-50">
+            <TaskUser></TaskUser>
+        </div>
+    </div>
     );
 }
 
